65d402f33cabc815fed2840dab6c14279526625c
Imane <imaneelmouradi98@gmail.com>
2019-04-29 19:50:27 +0100

    added details page ( products page and details are now linked )

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -12,30 +12,35 @@ class Product extends PureComponent {
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 mb-3">
         <div className="card">
-          <div
-            className="img-container p-5"
-            onClick={() => console.log("you clicked the img container")}
-          >
-            <Link to="/details">
-              <img src={img} alt="product" className="card-img-top" />
-            </Link>
+          <ProductConsumer>
+            {value => (
+              <div
+                className="img-container p-5"
+                onClick={() => value.handleDetail(id)}
+              >
+                <Link to="/details">
+                  <img src={img} alt="product" className="card-img-top" />
+                </Link>
 
-            <button
-              className="cart-btn"
-              disabled={inCart ? true : false}
-              onClick={() => {
-                console.log("added to the cart");
-              }}
-            >
-              {inCart ? (
-                <p className="text-capitalize mb-0" disabled>
-                  in Cart
-                </p>
-              ) : (
-                <i className="fa fa-cart-plus" />
-              )}
-            </button>
-          </div>
+                <button
+                  className="cart-btn"
+                  disabled={inCart ? true : false}
+                  onClick={() => {
+                    value.addToCart(id);
+                  }}
+                >
+                  {inCart ? (
+                    <p className="text-capitalize mb-0" disabled>
+                      in Cart
+                    </p>
+                  ) : (
+                    <i className="fa fa-cart-plus" />
+                  )}
+                </button>
+              </div>
+            )}
+          </ProductConsumer>
+
           {/* card footer */}
           <div className="card-footer d-flex justify-content-between">
             <p className="align-self-center mb-0">{title}</p>
--- src/context.jsx
+++ src/context.jsx
@@ -29,12 +29,20 @@ class ProductProvider extends Component {
     });
   };
 
-  handleDetail = () => {
-    console.log("hello from detail");
+  getItem = id => {
+    const product = this.state.products.find(item => item.id === id);
+    return product;
   };
 
-  addToCart = () => {
-    console.log("hello from add to cart");
+  handleDetail = id => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product };
+    });
+  };
+
+  addToCart = id => {
+    console.log(`hello from add to cart, the id is ${id}`);
   };
 
   render() {
